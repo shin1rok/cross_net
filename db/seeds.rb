5707c9532151dcbf1b578596b94742acc5104b82
shin1rok <skssikm@gmail.com>
2019-01-12 15:49:32 +0900

    サンプルデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# FIXME ここに書くべきではない
+Program.delete_all if Program.exists?
+Gymnasium.delete_all if Gymnasium.exists?
+30.times do |n|
+  Gymnasium.create!(name:       "体育館名#{n}",
+                    zip_code:   "0000000",
+                    prefecture: (0..46).to_a.sample,
+                    city:       "市町村区#{n}",
+                    street:     "番地#{n}",
+                    tell:       "0000000000",
+                    url:        "https://example.com/",
+                    access:     "JR田町駅から徒歩7分",
+                    court:      "10面",
+                    note:       "ここに備考を記入してください。")
+end
+
+Gymnasium.all.each do |gymnasium|
+  (1..3).to_a.sample.times do
+    gymnasium.programs.build(day:        (0..6).to_a.sample,
+                             start_time: '18:00',
+                             end_time:   '21:00').save!
+  end
+end
