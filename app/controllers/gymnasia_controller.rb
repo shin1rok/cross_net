4d9ab256d3260d0d90a3ad7771f9d2d94c660e8e
shin1rok <skssikm@gmail.com>
2019-03-12 23:27:19 +0900

    format

--- app/controllers/gymnasia_controller.rb
+++ app/controllers/gymnasia_controller.rb
@@ -15,24 +15,25 @@ class GymnasiaController < ApplicationController
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_gymnasium
-      @gymnasium = Gymnasium.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def gymnasium_params
-      params.require(:gymnasium).permit(:name,
-                                        :zip_code,
-                                        :prefecture_id,
-                                        :city,
-                                        :street,
-                                        :tell,
-                                        :url,
-                                        :access,
-                                        :court,
-                                        :note)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_gymnasium
+    @gymnasium = Gymnasium.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def gymnasium_params
+    params.require(:gymnasium).permit(:name,
+                                      :zip_code,
+                                      :prefecture_id,
+                                      :city,
+                                      :street,
+                                      :tell,
+                                      :url,
+                                      :access,
+                                      :court,
+                                      :note)
+  end
 
   def gymnasium_search_form
     params.fetch(:gymnasium_search_form, {}).permit(:prefecture_id)
